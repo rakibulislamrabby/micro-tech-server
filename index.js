03fcdd22b2c5155a454d23457d1105fc003a06ba
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-05-22 23:59:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsekk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected');
+        const collection = client.db("test").collection("devices");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Service is running")
+})
+app.listen(port, () => {
+    console.log('server is running');
+})
