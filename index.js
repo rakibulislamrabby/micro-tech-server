4114374599314c0338baf64d7a5d3a6ec9eb8808
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-05-24 00:28:21 +0600

    admin update

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
             res.send(users);
         })
 
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role == "admin";
+            res.send({ admin: isAdmin })
+        })
+
         //make admin api
         app.put("/user/admin/:email", verifyJwt, async (req, res) => {
             const email = req.params.email;
